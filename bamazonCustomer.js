31297befc0c52e730ae2934c66c3e2106b9ca6db
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-19 19:00:28 -0700

    added psudocoding comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,5 @@
+//First input npm requirements then connect to the mySQL database
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -15,5 +17,16 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    run();
+    runQuestions();
   });
+
+  //begin run  inquirer questions use last  in class activity for example
+
+  //will need to first ask user what product they would like--by name but reference by unique id
+  //use a switch case for both sets of questions????
+
+  // then ask user how much of the item they would like
+  // now deduct the user's chosen items from the inventory
+  // create a of out of stock back up error 
+
+  //once item is deducted from the inventory display item shipped and total cost
