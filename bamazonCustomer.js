1ee6236d5e3d9214e5bddcadcbea8de67fc7264c
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-24 13:21:16 -0700

    working inquirer questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 //First input npm requirements then connect to the mySQL database
 
 var mysql = require("mysql");
-//var inquirer = require("inquirer");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host:"localhost",
@@ -28,14 +28,33 @@ function displayItems() {
 
     console.log(res);
     connection.end();
+    promptBuy();
   });
 }
-  //begin run  inquirer questions use last  in class activity for example
-
+//begin run  inquirer questions use last  in class activity (great bay) for example
+function promptBuy() {
+  console.log("entered purchase function");
+  
   //will need to first ask user what product they would like--by name but reference by unique id
   //use a switch case for both sets of questions????
-
+inquirer.prompt([
+  {
+    type: "input",
+    name: "item_id",
+    message: "Please enter the ID Number of the item you wish to purchase.",
+    filter: Number
+  },
   // then ask user how much of the item they would like
+  //is there a way to set the default to one?
+  {
+    type: "input",
+    name: "quantity",
+message: "Now please select the quantity of the item you would like to order.",
+filter: Number
+  }
+])
+}
+
   // now deduct the user's chosen items from the inventory
   //
   // create a of out of stock back up error 
