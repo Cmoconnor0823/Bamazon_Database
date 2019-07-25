8e0660af0dc96adc82e67fad55fdf0b351675277
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-24 17:18:23 -0700

    added stop for negative inputs added bug program will not restart on negative input error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,15 +5,30 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
+  
   port: 3306,
-
+  
   user: "root",
-
+  
   password: "Stitch626",
   database: "bamazon_schema"
 });
 
+function validateInput(value) {
+  var integer = Number.isInteger(parseFloat(value));
+  var sign = Math.sign(value);
+
+
+  if (integer && (sign === 1)) {
+    return true;
+  } else {
+    return 'Error: Please input a non negative item quantity number to order.';
+    
+    ///some how the program needs to restart here????
+    runBamazonDB();
+    connection.end();
+  }
+}
 
 connection.connect(function (err) {
   if (err) throw err;
@@ -28,7 +43,7 @@ function runBamazonDB() {
 // display inventory items from bamazon database
 // show the item's id, the name and then the price
 function displayItems() {
-
+  
   //Create the database query quantity string
   queryQuantity = "Select * From products";
   //now make the query
@@ -36,7 +51,7 @@ function displayItems() {
     if (err) throw err;
     console.log("Current Inventory:  ");
     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
+    
     var queryOutput = "";
     for (var i = 0; i < data.length; i++) {
       queryOutput = "";
@@ -50,10 +65,11 @@ function displayItems() {
     promptBuy();
   });
 }
+
 //begin run  inquirer questions use last  in class activity (great bay) for example
 function promptBuy() {
   //console.log("~entered purchase function~");
-
+  
   //will need to first ask user what product they would like--by name but reference by unique id
   //use a switch case for both sets of questions????
   inquirer.prompt([
@@ -61,6 +77,7 @@ function promptBuy() {
       type: "input",
       name: "item_id",
       message: "Welcome to Bamazon's new ordering system!" + " Please enter the ID Number of the item you wish to purchase.",
+      validate: validateInput,
       filter: Number
     },
     // then ask user how much of the item they would like
@@ -69,63 +86,64 @@ function promptBuy() {
       type: "input",
       name: "quantity",
       message: "Now please select the quantity of the item you would like to order.",
+      validate: validateInput,
       filter: Number
     }
   ]).then(function (input) {
     //is there a way to show the name instead of the item's id?
     console.log('You have selected: \n    Selected Item id # = ' + input.item_id + '\n    quantity = ' + input.quantity);
-
+    
     var item = input.item_id;
     var quantity = input.quantity;
     //first validate the quantity with the db to ensure the order can be filled
     var queryQuantity = "Select * from Products where ?";
-
+    
     connection.query(queryQuantity, { item_id: item }, function (err, data) {
       if (err) throw err;
-
+      
       //create error condition for invalid id input
       if (data.length === 0) {
         console.log("An Error occured. Invalid Item Id. Please select a valid Item ID Number.");
         displayItems();
       } else {
         var productData = data[0]
-
+        
         //console.log('productData = ' + JSON.stringify(productData));
         //	console.log('productData.stock_quantity = ' + productData.stock_quantity);
-
+        
         // If the quantity requested by the user is in stock
         if (quantity <= productData.stock_quantity) {
           console.log("Congratulations, the Item you requested is in stock! Placing order...")
-
+          
           // now deduct the user's chosen items from the inventory
           //
           // Create the update stock function
           var updateQueryQuantity = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
           // console.log('updateQueryQuantity = ' + updateQueryQuantity);
-
+          
           // Update the inventory
           connection.query(updateQueryQuantity, function (err, data) {
             if (err) throw err;
-
+            
             //once item is deducted from the inventory display item shipped and total cost
             console.log('Your order has been placed! Your total is $' + productData.price * quantity);
             console.log('Thank you for shopping at Bamazon_db!');
             console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-
-
+            
+            
             ////instead of end cpnnectipm here can i ask the user if they
             //want to place a second order
             // End the database connection
             //connection.end();
             repromptUser();
-
+            
           })
           // create a of out of stock back up error 
         } else {
           console.log('Sorry, there is not enough of that item in stock, your order can not be placed.');
           console.log('Please re-select your order.');
           console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-
+          
           displayItems();
         }
       }
@@ -143,7 +161,8 @@ function repromptUser(){
     if(ans.reply){
       runBamazonDB();
     } else{
-      console.log("See you soon!");
+      console.log("See you again soon! Thank you for choosing Bamazon!");
+      connection.end();
     }
   });
 }
